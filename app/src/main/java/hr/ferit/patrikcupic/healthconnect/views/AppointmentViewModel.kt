72b2b153a6d56cb4e576fd7c4b01a51cc352f851
pcupic <cupicpatrik@gmail.com>
2025-01-17 21:38:46 +0100

    remove appointment view model from other views and classes

--- app/src/main/java/hr/ferit/patrikcupic/healthconnect/views/AppointmentViewModel.kt
+++
@@ -1,7 +0,0 @@
-package hr.ferit.patrikcupic.healthconnect.views
-
-import hr.ferit.patrikcupic.healthconnect.data.Appointment
-
-interface AppointmentViewModel {
-    val appointmentsData: MutableList<Appointment>
-}
